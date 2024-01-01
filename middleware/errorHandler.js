a83b72acc8b2bf1caf141516d8e1ed29a626ee40
Rupam Das <rejisterjack@gmail.com>
2024-01-02 00:44:02 +0530

    modified error handler

--- middleware/errorHandler.js
+++ middleware/errorHandler.js
@@ -2,7 +2,24 @@ const errorHandler = (err, req, res, next) => {
   const statusCode = req.statusCode ? req.statusCode : 500
   switch (statusCode) {
     case 400:
-      res.json({ message: err.message, stackTrace: err.stack })
+      res.json({
+        title: "Validation Error",
+        message: err.message,
+        stackTrace: err.stack,
+      })
       break
+    case 404:
+      res.json({
+        title: "Not Found",
+        message: err.message,
+        stackTrace: err.stack,
+      })
+      break
+    default:
+      res.json({
+        title: "Unknown Error",
+        message: err.message,
+        stackTrace: err.stack,
+      })
   }
 }
